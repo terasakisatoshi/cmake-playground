17abc6929a694aef817421b12a9dd6ac4bfd8158
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-05-06 15:59:29 +0900

    remove Julia_PREFIX

--- cxxwrap2/build.sh
+++ cxxwrap2/build.sh
@@ -1,6 +1,6 @@
 # Get Julia installation paths
 JULIA_PREFIX=`julia --project -e 'joinpath(Sys.BINDIR, "..") |> abspath |> print'`
 CXXWRAP_PREFIX=`julia --project -e 'using CxxWrap; CxxWrap.prefix_path() |> print'`
-cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release -DJulia_PREFIX=$JULIA_PREFIX -DCMAKE_PREFIX_PATH=$CXXWRAP_PREFIX
+cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$CXXWRAP_PREFIX
 cmake --build ./build --config Release -j `nproc`
 
