f3da3f19547862a041f1b0d9253eb11362413a07
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-05-06 14:49:05 +0900

    add workflow for cxxwrap2

--- cxxwrap2/build.sh
+++ cxxwrap2/build.sh
@@ -1,26 +1,7 @@
-# Detect operating system
-OS_TYPE=$(uname -s)
-
-# Set shared library extension based on OS
-if [[ $OS_TYPE == "Linux" ]]; then
-  SHARED_LIB_EXT=".so"
-elif [[ $OS_TYPE == "Darwin" ]]; then
-  SHARED_LIB_EXT=".dylib"
-else
-  echo "Unsupported operating system: $OS_TYPE"
-  exit 1
-fi
-
 # Get Julia installation paths
 JULIA_PREFIX=`julia --project -e 'joinpath(Sys.BINDIR, "..") |> abspath |> print'`
 CXXWRAP_PREFIX=`julia --project -e 'using CxxWrap; CxxWrap.prefix_path() |> print'`
-cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release -DJulia_PREFIX=$JULIA_PREFIX -DCMAKE_PREFIX_PATH=$CXXWRAP_PREFIX
+# cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release -DJulia_PREFIX=$JULIA_PREFIX -DCMAKE_PREFIX_PATH=$CXXWRAP_PREFIX
+cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release
 cmake --build ./build --config Release -j `nproc`
 
-# Build shared library with appropriate extension
-#g++ -fPIC -shared -std=c++17 \
-#  -I${CXXWRAP_PREFIX}/include/ \
-#  -L${CXXWRAP_PREFIX}/lib/ \
-#  -I${JULIA_PREFIX}/include/julia \
-#  -L${JULIA_PREFIX}/lib \
-#  -ljulia -lcxxwrap_julia hello.cpp -o libhello${SHARED_LIB_EXT}
