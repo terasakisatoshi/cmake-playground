af03e7222aaba0337309fcdf6a4ff32471b1445c
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-06-22 12:18:11 +0900

    fix

--- cxxwrap3/build.sh
+++ cxxwrap3/build.sh
@@ -1,4 +1,5 @@
 # Get Julia installation paths
+julia --project -e 'using Pkg; Pkg.instantiate()'
 CXXWRAP_PREFIX=`julia --project -e 'using CxxWrap; CxxWrap.prefix_path() |> print'`
 cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$CXXWRAP_PREFIX
 cmake --build ./build --config Release -j `nproc`
