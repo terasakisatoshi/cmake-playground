637284a3a39c068cb32deb68ed789e61f0789ea4
terasaki <terasakisatoshi.math@gmail.com>
2024-07-01 16:28:28 +0900

    fix

--- barnsley_fern/build.sh
+++ barnsley_fern/build.sh
@@ -1,5 +1,6 @@
 # Get Julia installation paths
-julia --project -e 'using Pkg; Pkg.instantiate()'
+# julia --project -e 'using Pkg; Pkg.instantiate()'
+julia -e 'using Pkg; Pkg.activate(); Pkg.add("CxxWrap")'
 CXXWRAP_PREFIX=`julia --project -e 'using CxxWrap; CxxWrap.prefix_path() |> print'`
 cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$CXXWRAP_PREFIX
 cmake --build ./build --config Release -j `nproc`
